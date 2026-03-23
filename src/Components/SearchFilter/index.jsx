fca98d4c0c4fa776186d60f42849e4f38302104e
Muxriddin Toshboyev <167794484+MukhriddinToshboyev@users.noreply.github.com>
2026-03-23 15:39:38 +0500

    Add SearchCards component to Home page

---
+++ src/Components/SearchFilter/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SearchCards = () => {
+  return <div>SearchCards</div>;
+};
+
+export default SearchCards;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import Navigation from "../Components/Navigation";
+import SearchCards from "../Components/SearchFilter";
 
 function Home() {
   return (
     <div>
       <Navigation />
+      <SearchCards />
     </div>
   );
 }
